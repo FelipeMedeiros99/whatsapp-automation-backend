3d8dee2cb70d137f8f5a7433adb277797588a120
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-07-29 02:56:08 -0300

    feat: prisma db added

---
+++ src/config/index.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from '../generated/prisma'
+
+const prisma = new PrismaClient()
+
+export const updateMessage = async(id: number, message: string) =>{
+  try{
+    const update = await prisma.defaultMessage.update({where: {id: id}, data: {message: message}});
+    return update;
+  }catch(e){
+    return(e)
+  }
+}
+
+export const getAllMessages = async() => {
+  try{
+    const messages = await prisma.defaultMessage.findMany();
+    return messages;
+  }catch(e){
+    return e
+  }
+}
+
+export const getMessageByTitle = async(title: string) => {
+  try{
+    const message = await prisma.defaultMessage.findFirst({where:{title: title}})
+    return message;
+  }catch(e){
+    return e
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,100 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
-import {Request, Response, NextFunction} from "express"
-import {client} from "./client"
+import { Request, Response, NextFunction } from "express"
+import { client } from "./client"
 
 import 'dotenv/config';
+import { getAllMessages, getMessageByTitle, updateMessage } from "./config";
 
 const app = express()
 app.use(cors())
+app.use(json())
 const port = process.env.PORT
 
-app.get("/whatsapp/connect/", async(req: Request, res: Response)=>{
-  try{
+app.get("/whatsapp/connect/", async (req: Request, res: Response) => {
+  try {
     const qrCode = await client.connect()
     res.send(qrCode);
-  }catch(e){
+    return
+  } catch (e) {
     res.send(e).status(500)
+    return
   }
 });
 
-app.get("/whatsapp/reconnect/", async(req: Request, res: Response)=>{
-  try{
+app.get("/whatsapp/reconnect/", async (req: Request, res: Response) => {
+  try {
     const qrCode = await client.generateAnoterQrCode()
     res.send(qrCode);
-  }catch(e){
+    return
+  } catch (e) {
     res.send(e).status(500)
+    return
   }
 });
 
-app.get("/whatsapp/desconnect/", async(req: Request, res: Response)=>{
-  try{
+app.get("/whatsapp/desconnect/", async (req: Request, res: Response) => {
+  try {
     const qrCode = await client.generateAnoterQrCode()
     res.send(qrCode);
-  }catch(e){
+    return
+  } catch (e) {
     res.send(e).status(500)
+    return
   }
 });
 
 
-app.get("/whatsapp/status/", async(req: Request, res: Response)=>{
-  try{
+app.get("/whatsapp/status/", async (req: Request, res: Response) => {
+  try {
     const qrCode = client.getStatus()
     res.send(qrCode);
-  }catch(e){
+    return
+  } catch (e) {
     res.send(e).status(500)
+    return
   }
 });
 
-app.listen(port, ()=>{
+app.get("/whatsapp/messages/", async (req: Request, res: Response) => {
+  try {
+    const messages = await getAllMessages();
+    res.send(messages);
+    return
+  } catch (e) {
+    res.send(e).status(500)
+    return
+  }
+})
+
+app.get("/whatsapp/messages/:title", async (req: Request, res: Response) => {
+  const title = req.params.title
+  try {
+    const messages = await getMessageByTitle(title);
+    res.send(messages);
+    return
+  } catch (e) {
+    res.send(e).status(500)
+    return
+  }
+})
+
+app.put("/whatsapp/messages/update/", async (req: Request, res: Response) => {
+
+  console.log(req.body)
+  const { message, id } = req.body;
+  console.log(id)
+  try{
+    const update = await updateMessage(id, message);
+    res.send(update)
+    return
+  }catch(e){
+    res.send(e).status(500)
+    return
+  }
+})
+
+
+app.listen(port, () => {
   console.log(`server running at port ${port}`);
 })
