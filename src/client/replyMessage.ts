72f28f44b261889581c19dcd72bf693f8d6bfa14
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-10-31 06:01:04 -0300

    fix: stop settings added

--- src/client/replyMessage.ts
+++ src/client/replyMessage.ts
@@ -71,6 +71,7 @@ export default async function replyMessage(message: Message, client: Client) {
 
   if (!isMe && messageFrom.includes("@c.us") && contentMessage) {
     const restrictionDelayPromise = prisma.restrictions.findUnique({where: {title: "responseDelay"}})
+    const transferPhrasePromise = prisma.restrictions.findUnique({where: {title: "responseDelay"}})
 
     const clientChatId = messageFrom;
     const userData = await createUser({
@@ -91,7 +92,7 @@ export default async function replyMessage(message: Message, client: Client) {
       
       const restrictionDelay = await restrictionDelayPromise;
       let delay = Number(restrictionDelay?.restrictionNumber) * 1000 || 3500;
-
+      
       const userTimeout = setTimeout(async () => {
         try {
           const messagesDataPromise = findMessage(clientChatId)
@@ -99,15 +100,17 @@ export default async function replyMessage(message: Message, client: Client) {
           const [messagesData] = await Promise.all([messagesDataPromise, deleteTimeoutIdPromise]);
           
           if (!messagesData) return;
-    
+          
           const messageContext = messagesData?.map((msg: { from: string, text: string }) => `${msg.from}: ${msg.text}`).join("\n")
           const geminiResponsePromise = geminiResponse(messageContext) || ""
-
+          
           if (messagesData[messagesData.length - 1]?.from === "client") {
             const responseFromGemini = await geminiResponsePromise || "";
             const sendingMessage = sendMessage(responseFromGemini);
+            
+            let transferPhrase = await transferPhrasePromise
 
-            if (responseFromGemini.toLocaleLowerCase().includes("irei repassar você para um atendente")) {
+            if (responseFromGemini.toLocaleLowerCase().includes(transferPhrase?.restriction || "irei repassar você para um atendente")) {
               const updateuserPromise = updateUser(clientChatId, {isBotStoped: true, lastMessageFromBot: true})
               await Promise.all([updateuserPromise, sendingMessage])
               return;
