4e7d487a0057b1564fa8447d1219c32d92ca021c
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2026-08-08 15:20:43 -0300

    feat: add environment configuration and update package dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,52 @@
 import express, { json } from "express";
 import cors from "cors";
-import 'dotenv/config';
+import "dotenv/config";
 
-import {connectController, disconnectClientController, getStatusController } from "./controllers/conectionControllers.js";
-import { getRestrictionController, updateRestrictionController } from "./controllers/geminiControllers.js";
+import {
+  connectController,
+  disconnectClientController,
+  getStatusController,
+} from "./controllers/conectionControllers.js";
+import {
+  getRestrictionController,
+  updateRestrictionController,
+} from "./controllers/geminiControllers.js";
 
 import { handleErrorMiddleware } from "./middlewares/handleErrorMiddleware.js";
 import { createRestrictionsDefault } from "./tools/automaticCreations.js";
-import { addDefaultMessage, deleteDefaultMessage, getDefaultMessages, updateMessages } from "./controllers/defaultMessagesCotrollers.js";
+import {
+  addDefaultMessage,
+  deleteDefaultMessage,
+  getDefaultMessages,
+  updateMessages,
+} from "./controllers/defaultMessagesCotrollers.js";
 
-const PORT = process.env.PORT || 5002
-const app = express()
-app.use(cors())
-app.use(json())
+const PORT = process.env.PORT || 5002;
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/whatsapp/connect/", connectController);
 app.get("/whatsapp/disconnect/", disconnectClientController);
 app.get("/whatsapp/status/", getStatusController);
 
-app.get("/whatsapp/restriction/", getRestrictionController)
-app.put("/whatsapp/restriction/:id", updateRestrictionController)
+app.get("/whatsapp/restriction/", getRestrictionController);
+app.put("/whatsapp/restriction/:id", updateRestrictionController);
 
-app.get("/whatsapp/default_messages/", getDefaultMessages)
-app.put("/whatsapp/default_messages/:id", updateMessages)
-app.post("/whatsapp/default_messages/", addDefaultMessage)
-app.delete("/whatsapp/default_messages/:id", deleteDefaultMessage)
-
+app.get("/whatsapp/default_messages/", getDefaultMessages);
+app.put("/whatsapp/default_messages/:id", updateMessages);
+app.post("/whatsapp/default_messages/", addDefaultMessage);
+app.delete("/whatsapp/default_messages/:id", deleteDefaultMessage);
 
 app.use(handleErrorMiddleware);
 
-(async()=>{
+(async () => {
   // setting database restrictions
-  
+
   await createRestrictionsDefault();
   app.listen(Number(PORT), async () => {
     console.log(`server running at port ${PORT}`);
-  })
-})()
+  });
+})();
