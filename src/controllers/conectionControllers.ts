b49aff9ba58f8dbddf951fb5d0f4d7b3e0f77d32
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2026-09-14 22:01:18 -0300

    refactor: renaming client folder

--- src/controllers/conectionControllers.ts
+++ src/controllers/conectionControllers.ts
@@ -1,38 +1,38 @@
-import { client } from "../client/index.js";
-import { 
-  Request, 
-  Response 
-} from "express";
-import service2 from "../client_2/index.js";
+import { Request, Response } from "express";
+import service2 from "../client/index.js";
 
-export async function connectController(req: Request, res: Response){
+export async function connectController(req: Request, res: Response) {
   try {
     const qrCode = await service2.getQrCode();
-    console.log(qrCode)
-    res.send(qrCode)
-    return
-  } catch (e) {
-    throw {message: "Erro ao se conectar", statusCode: 500, error: e}
-  }
-}
-
-export async function getStatusController(req: Request, res: Response){
-  try {
-    // const status = client.getStatus()
-    const state = service2.isLoged
-    res.send({isLoged: state});
+    console.log(qrCode);
+    res.send(qrCode);
     return;
   } catch (e) {
-    throw {message: "Erro ao buscar status", statusCode: 500, error: e}
+    throw { message: "Erro ao se conectar", statusCode: 500, error: e };
   }
 }
 
-export async function disconnectClientController(req: Request, res: Response){
+export async function getStatusController(req: Request, res: Response) {
   try {
-    const disconnectResponse = await service2.destroyClient()
+    // const status = client.getStatus()
+    const state = service2.isLoged;
+    res.send({ isLoged: state });
+    return;
+  } catch (e) {
+    throw { message: "Erro ao buscar status", statusCode: 500, error: e };
+  }
+}
+
+export async function disconnectClientController(req: Request, res: Response) {
+  try {
+    const disconnectResponse = await service2.destroyClient();
     res.send(disconnectResponse);
     return;
   } catch (e) {
-    throw {message: "Erro ao desconectar whatsapp", statusCode: 500, error: e}
+    throw {
+      message: "Erro ao desconectar whatsapp",
+      statusCode: 500,
+      error: e,
+    };
   }
 }
